9e56baa9ead5163886fce61897e927e45d7358f9
h33 <h33je5sg@gmail.com>
2014-08-31 17:34:37 +0900

    work first

---
+++ UnityPingPong/Assets/Scripts/Ball.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Physics.gravity = Vector3.zero;
+		Vector3 force = new Vector3(0f,100f,-1000f );
+		gameObject.rigidbody.AddForce( force );
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ UnityPingPong/Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	private float positionZ = -10;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// MousePosition
+		Vector3 mousePosition = Input.mousePosition;
+		mousePosition.z = 5;
+		// CameraPosition
+		Vector3 positionOnScreen = Camera.main.ScreenToWorldPoint( mousePosition );
+
+		print ( positionOnScreen );
+
+		gameObject.transform.position = positionOnScreen;
+	}
+}
