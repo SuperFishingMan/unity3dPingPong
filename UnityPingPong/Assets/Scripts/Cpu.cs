2132c3cb430f445d0bb653f9fb63b7cd7821e455
h33 <h33je5sg@gmail.com>
2014-09-13 15:45:08 +0900

    temporary

---
+++ UnityPingPong/Assets/Scripts/Cpu.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cpu : Player {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log( this );
+	}
+	
+	public override void Update(){
+		// base.Update ();
+		// Debug.Log (GameObject.Find ("Ball"));
+		// gameObject.transform.position = new Vector3( 
+	}
+
+
+}
---
+++ UnityPingPong/Assets/Scripts/Player2.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player2 : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GameObject ball = GameObject.Find ("Ball");
+		gameObject.transform.position = new Vector3 (ball.transform.position.x, ball.transform.position.y);
+	}
+}
