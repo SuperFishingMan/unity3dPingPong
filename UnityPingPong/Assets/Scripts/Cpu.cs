901d80fe4cb3150482534c2bdd8ef70d3f79dc12
h33 <h33je5sg@gmail.com>
2014-09-17 03:45:32 +0900

    added title, update cpu

--- UnityPingPong/Assets/Scripts/Cpu.cs
+++ UnityPingPong/Assets/Scripts/Cpu.cs
@@ -3,15 +3,28 @@ using System.Collections;
 
 public class Cpu : Player {
 
+	private GameObject ball;
+	private float startZ = 0;
+
+	private int status = 0;
+
 	// Use this for initialization
 	void Start () {
 		Debug.Log( this );
+		ball = GameObject.Find("Ball");
+		startZ = gameObject.transform.position.z;
 	}
 	
 	public override void Update(){
 		// base.Update ();
 		// Debug.Log (GameObject.Find ("Ball"));
 		// gameObject.transform.position = new Vector3( 
+
+		gameObject.transform.position = new Vector3( ball.transform.position.x, ball.transform.position.y, startZ );
+
+
+		// switch status
+
 	}
 
 
